36ab7c3d84095f2331c656192f9618e4357bd81e
LunarBitz <atmosphir44@gmail.com>
2020-05-18 23:11:51 -0500

    Themed Colors
    - Adding colors as resource map entries
    - - For easy theme changing / user made themes later on

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -25,12 +25,56 @@ namespace BitzDrawingFileCreator_WPF
         {
             InitializeComponent();
 
-            this.Resources["thmclrMainText"] = (Brush)(new BrushConverter().ConvertFrom("#FF" + "c000c0"));
+            Resources["themeclr_Text_Primary"]              = (Brush)(new BrushConverter().ConvertFrom("#FF" + "c000c0"));
+            Resources["themeclr_Text_Secondary"]            = (Brush)(new BrushConverter().ConvertFrom("#FF" + "5f2568"));
+            Resources["themeclr_Text_Tertiary"]             = (Brush)(new BrushConverter().ConvertFrom("#FF" + "341d38"));
+
+            Resources["themeclr_Button_Text"]               = Resources["themeclr_Text_Primary"];
+            Resources["themeclr_Button_Background"]         = (Brush)(new BrushConverter().ConvertFrom("#FF" + "0f0f0f"));
+            Resources["themeclr_Button_Border"]             = (Brush)(new BrushConverter().ConvertFrom("#00" + "000000"));
+            Resources["themeclr_Button_Hover"]              = (Brush)(new BrushConverter().ConvertFrom("#FF" + "404040"));
+            Resources["themeclr_Button_Clicked"]            = (Brush)(new BrushConverter().ConvertFrom("#FF" + "79517d"));
+            Resources["themeclr_Button_Selected"]           = (Brush)(new BrushConverter().ConvertFrom("#FF" + "c0c0c0"));
+
+            Resources["themeclr_Menu_Background"]           = (Brush)(new BrushConverter().ConvertFrom("#FF" + "0f0f0f"));
+            Resources["themeclr_MenuOption_Text"]           = Resources["themeclr_Text_Primary"];
+            Resources["themeclr_MenuOption_Background"]     = (Brush)(new BrushConverter().ConvertFrom("#00" + "000000"));
+            Resources["themeclr_MenuOption_Border"]         = (Brush)(new BrushConverter().ConvertFrom("#00" + "000000"));
+            Resources["themeclr_MenuOption_Hover"]          = (Brush)(new BrushConverter().ConvertFrom("#FF" + "404040"));
+            Resources["themeclr_MenuOption_Clicked"]        = (Brush)(new BrushConverter().ConvertFrom("#FF" + "79517d"));
+            Resources["themeclr_MenuOption_Selected"]       = (Brush)(new BrushConverter().ConvertFrom("#FF" + "c0c0c0"));
+
+            Resources["themeclr_SubMenu_Background"]        = (Brush)(new BrushConverter().ConvertFrom("#FF" + "232027"));
+            Resources["themeclr_SubMenuOption_Text"]        = Resources["themeclr_Text_Primary"];
+            Resources["themeclr_SubMenuOption_Background"]  = (Brush)(new BrushConverter().ConvertFrom("#00" + "000000"));
+            Resources["themeclr_SubMenuOption_Border"]      = (Brush)(new BrushConverter().ConvertFrom("#00" + "000000"));
+            Resources["themeclr_SubMenuOption_Hover"]       = (Brush)(new BrushConverter().ConvertFrom("#FF" + "404040"));
+            Resources["themeclr_SubMenuOption_Clicked"]     = (Brush)(new BrushConverter().ConvertFrom("#FF" + "79517d"));
+            Resources["themeclr_SubMenuOption_Selected"]    = (Brush)(new BrushConverter().ConvertFrom("#FF" + "c0c0c0"));
+
+            Resources["themeclr_EntryText_Text"]            = Resources["themeclr_Text_Primary"];
+            Resources["themeclr_EntryText_Background"]      = (Brush)(new BrushConverter().ConvertFrom("#FF" + "c000c0"));
+            Resources["themeclr_EntryTex_Border"]           = (Brush)(new BrushConverter().ConvertFrom("#FF" + "c000c0"));
+
+            Resources["themeclr_ListBox_Text"]              = Resources["themeclr_Text_Primary"];
+            Resources["themeclr_ListBox_Background"]        = (Brush)(new BrushConverter().ConvertFrom("#FF" + "c000c0"));
+            Resources["themeclr_ListBox_Border"]            = (Brush)(new BrushConverter().ConvertFrom("#FF" + "c000c0"));
+
+            Resources["themeclr_ComboBox_Text"]             = Resources["themeclr_Text_Primary"];
+            Resources["themeclr_ComboBox_Arrow"]            = (Brush)(new BrushConverter().ConvertFrom("#FF" + "c000c0"));
+            Resources["themeclr_ComboBox_Background"]       = (Brush)(new BrushConverter().ConvertFrom("#FF" + "c000c0"));
+            Resources["themeclr_ComboBox_Border"]           = (Brush)(new BrushConverter().ConvertFrom("#FF" + "c000c0"));
+
+            Resources["themeclr_Scroll_Thumb"]              = Resources["themeclr_Text_Primary"];
+            Resources["themeclr_Scroll_Arrows"]             = (Brush)(new BrushConverter().ConvertFrom("#FF" + "c000c0"));
+            Resources["themeclr_Scroll_Background"]         = (Brush)(new BrushConverter().ConvertFrom("#FF" + "c000c0"));
+            Resources["themeclr_Scroll_Border"]             = (Brush)(new BrushConverter().ConvertFrom("#FF" + "c000c0"));
+
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            this.Resources["thmclrMainText"] = (Brush)(new BrushConverter().ConvertFrom("#FF" + "00FFFF"));
+            Resources["themeclr_Button_Text"] = (Brush)(new BrushConverter().ConvertFrom("#FF" + "00FFFF"));
         }
     }
 }

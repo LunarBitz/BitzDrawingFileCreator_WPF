3fce56cf5e6269cac4ab73b57e7bf7ab370eca16
LunarBitz <atmosphir44@gmail.com>
2020-06-01 02:05:49 -0500

    Fixed Folder Creation ADD

--- Data/AppData.cs
+++ Data/AppData.cs
@@ -8,10 +8,30 @@ namespace BitzDrawingFileCreator_WPF.Data
 {
     public class AppData : ObservableObject
     {
-
+        
         private Dictionary<string, string> privateVars = new Dictionary<string, string>();
 
         #region Entry Data
+        public string drawingDescription
+        {
+            get
+            {
+                string baseName = System.Reflection.MethodBase.GetCurrentMethod().Name.Replace("get_", "");
+
+                if (!privateVars.ContainsKey(baseName))
+                    return "";
+                if (string.IsNullOrEmpty(privateVars[baseName]))
+                    return "";
+                return privateVars[baseName];
+            }
+            set
+            {
+                string baseName = System.Reflection.MethodBase.GetCurrentMethod().Name.Replace("set_", "");
+
+                privateVars[baseName] = value;
+                OnPropertyChanged(baseName);
+            }
+        }
         public string userName 
         {
             get

ad0ec6feafd0e537290198f5919c7567e387470f
LunarBitz <atmosphir44@gmail.com>
2020-11-28 01:36:58 -0600

    Reworking binding

---
+++ Data/UserInfo.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitzDrawingFileCreator_WPF.Data
+{
+    public class UserInfo : ObservableObject
+    {
+        private Dictionary<string, string> privateVars = new Dictionary<string, string>();
+
+        #region Entry Data
+        public string trelloBoardID
+        {
+            get
+            {
+                string baseName = System.Reflection.MethodBase.GetCurrentMethod().Name.Replace("get_", "");
+
+                if (!privateVars.ContainsKey(baseName))
+                    return "";
+                if (string.IsNullOrEmpty(privateVars[baseName]))
+                    return "";
+                return privateVars[baseName];
+            }
+            set
+            {
+                string baseName = System.Reflection.MethodBase.GetCurrentMethod().Name.Replace("set_", "");
+
+                privateVars[baseName] = value;
+                OnPropertyChanged(baseName);
+            }
+        }
+        public string trelloDefaultList
+        {
+            get
+            {
+                string baseName = System.Reflection.MethodBase.GetCurrentMethod().Name.Replace("get_", "");
+
+                if (!privateVars.ContainsKey(baseName))
+                    return "";
+                if (string.IsNullOrEmpty(privateVars[baseName]))
+                    return "";
+                return privateVars[baseName];
+            }
+            set
+            {
+                string baseName = System.Reflection.MethodBase.GetCurrentMethod().Name.Replace("set_", "");
+
+                privateVars[baseName] = value;
+                OnPropertyChanged(baseName);
+            }
+        }
+        public string trelloApiKey
+        {
+            get
+            {
+                string baseName = System.Reflection.MethodBase.GetCurrentMethod().Name.Replace("get_", "");
+
+                if (!privateVars.ContainsKey(baseName))
+                    return "";
+                if (string.IsNullOrEmpty(privateVars[baseName]))
+                    return "";
+                return privateVars[baseName];
+            }
+            set
+            {
+                string baseName = System.Reflection.MethodBase.GetCurrentMethod().Name.Replace("set_", "");
+
+                privateVars[baseName] = value;
+                OnPropertyChanged(baseName);
+            }
+        }
+        public string trelloToken
+        {
+            get
+            {
+                string baseName = System.Reflection.MethodBase.GetCurrentMethod().Name.Replace("get_", "");
+
+                if (!privateVars.ContainsKey(baseName))
+                    return "";
+                if (string.IsNullOrEmpty(privateVars[baseName]))
+                    return "";
+                return privateVars[baseName];
+            }
+            set
+            {
+                string baseName = System.Reflection.MethodBase.GetCurrentMethod().Name.Replace("set_", "");
+
+                privateVars[baseName] = value;
+                OnPropertyChanged(baseName);
+            }
+        }
+        #endregion
+
+    }
+}
